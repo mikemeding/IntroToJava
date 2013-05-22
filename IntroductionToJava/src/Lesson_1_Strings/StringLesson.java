771206cca3e1489f9d87507669a49652ccec3c25
mikemeding <mikeymeding@gmail.com>
2013-05-21 20:25:08 -0400

    first commit.

---
+++ IntroductionToJava/src/Lesson_1_Strings/StringLesson.java
@@ -0,0 +1,16 @@
+package Lesson_1_Strings;
+
+/**
+ *
+ * @author mike
+ */
+public class StringLesson {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
